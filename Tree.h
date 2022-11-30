343031c9a1663f2e6168192126f26cee0d3c1ded
GJC <332982539@qq.com>
2022-11-30 15:28:22 +0800

    mergin

---
+++ Tree.h
@@ -0,0 +1,19 @@
+//
+// Created by GJC on 2022/11/30.
+//
+
+#ifndef DATASTRUCTUREEXERCISE_TREE_H
+#define DATASTRUCTUREEXERCISE_TREE_H
+
+#include <iostream>
+#include "Define.h"
+
+using namespace std;
+
+typedef struct BiTNode{
+
+
+}
+
+
+#endif //DATASTRUCTUREEXERCISE_TREE_H
